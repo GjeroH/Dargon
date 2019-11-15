2bef1782d9ce0d6f1495296b9a84c7a198ca7a4d
Hawking Hook <hawking.gh@gmail.com>
2019-11-15 16:16:21 +1000

    Ask for player name

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -11,12 +11,18 @@ namespace I_DO_NOT_KNOW
         static void Main(string[] args)
         {
 
-            // Declare variable for player answers
+            // Declare variable for player name and answers
+            string playerName;
             string playerAnswer = "nil";
 
             // Start of adventure
-            Console.WriteLine("While walking through a dark forest you come across a cave entrance. " +
-                                "\n\nIt's starting to rain and it's nearly night.\n\nDo you enter?");
+            Console.Write("Welcome to my text adventure.\n--------------------------------------------------\nWhat is your name? ");
+            playerName = Console.ReadLine();
+
+
+            Console.WriteLine("While walking through a dark forest the brave adventurer " + playerName +
+                              " came across a cave entrance. " +
+                              "\n\nIt started to rain and was nearly night.\n\nDid " + playerName + " enter?");
 
             // Ask for player answer and store in variable
             playerAnswer = Console.ReadLine();
@@ -24,18 +30,18 @@ namespace I_DO_NOT_KNOW
             // What happens to player based on answer
             if (playerAnswer == "yes")
             {
-                Console.WriteLine("You have survived.");
+                Console.WriteLine(playerName + " has survived.");
             }
             else
             {
-                Console.WriteLine("You have died of hypothermia in the rain.");
+                Console.WriteLine(playerName + " has died of hypothermia in the rain.");
                 Console.Beep();
                 Console.Beep();
                 Console.Beep();
             }
 
             // Ask player what they want to do now
-            Console.WriteLine("Would you like to play again?");
+            Console.Write("--------------------------------------------------\nWould you like to play again? ");
             Console.ReadKey();
         }
     }
